8efa075fa970b4b10c4722c3645e368409ba0027
jhavidit <jhavidit@gmail.com>
2021-05-24 13:37:10 +0530

    init project

---
+++ app/src/main/java/tech/jhavidit/custom_bottom_nav/MainActivity.kt
@@ -0,0 +1,11 @@
+package tech.jhavidit.custom_bottom_nav
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
